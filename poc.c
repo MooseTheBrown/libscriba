660bdbeb08f4b5df1e296ca048182a4156b6ba1a
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2014-05-13 15:54:55 +0400

    LGPL

--- poc.c
+++ poc.c
@@ -1,3 +1,23 @@
+/* 
+ * Copyright (C) 2014 Mikhail Sapozhnikov
+ *
+ * This file is part of libscriba.
+ *
+ * libscriba is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libscriba is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libscriba. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "poc.h"
 #include "db_backend.h"
 #include <stdlib.h>
